cb17e1f7f6422ea8b813eada8dc40399f8a73ec6
XoanCarreira <154058163+XoanCarreira@users.noreply.github.com>
2024-06-15 21:10:21 +0200

    commit correct icons size

--- src/routes/components/Popup.svelte
+++ src/routes/components/Popup.svelte
@@ -27,6 +27,8 @@
                 <li>É moi importante non deixar ningún tipo de residuo</li>
                 <li>Non debemos cortar nin maltratar árbores, xa que ademais do respeto á natureza, moitos terreos son privados e podería ter consecuencias negativas para a nosa actividade</li>
                 <li>Non molestes a fauna, eles estaban antes</li>
+                <li>A escalada é un deporte de risco no que cada un é responsable de garantir a súa seguridade</li>
+                <li>Boulder Compostela non se fai responsable de posibles accidentes durante a práctica da escalada nas zonas reseñadas</li>
             </ul>
 			<button on:click={closeModal}>Cerrar</button>
 		</div>
